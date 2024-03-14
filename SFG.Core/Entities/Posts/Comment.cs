c469964c7675de4ff80b3354a1dc558f9d17f8d2
songdt <songdt@meperia.com>
2024-03-14 15:40:59 +0700

    commit
    Signed-off-by: songdt <songdt@meperia.com>

--- SFG.Core/Entities/Posts/Comment.cs
+++ SFG.Core/Entities/Posts/Comment.cs
@@ -8,7 +8,7 @@ namespace SFG.Core.Entities.Posts
 	{
         public int Id { get; set; }
 		public string Content { get; set; } = string.Empty;
-        public string CreatedBy { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public bool IsDeleted { get; set; }
